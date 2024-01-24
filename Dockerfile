badac6cabd6f9e9852859d31404f826283e863c0
Kuzminova Olga <kuzminovaforwork@gmail.com>
2024-01-24 11:14:11 +0300

    mod_rewrite

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM php:8.0-apache
 RUN docker-php-ext-install mysqli
 COPY messagesapp.conf /usr/local/apache2/conf/messagesapp.conf
-
+COPY .htaccess /var/www/html
+RUN echo "LoadModule rewrite_module modules/mod_rewrite.so" >> /usr/local/apache2/conf/httpd.conf
+RUN a2enmod rewrite 
--- messagesapp/index.php
+++ messagesapp/index.php
@@ -7,13 +7,14 @@ use messagesapp\Database;
 use messagesapp\ViewMessages;
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+echo $path;
 
 
 if (!preg_match('~^\/(\?page=(\d+))?$~', $path)) {
     echo 'routes.php'; 
     require 'routes.php';
     
-    
+
 } elseif (preg_match('~^\/view_message\.php\?id=\d+$~', $path)) {
     echo 'вариант 2';
 } else {
